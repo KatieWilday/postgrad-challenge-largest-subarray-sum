35ed94ae3122787fc9ef224b7ed85ed605d553c2
Katie Wilday <wildayk@gmail.com>
2020-04-16 11:24:32 -0400

    return largestSum

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let array = [1, 3, -5, 15, -34, 90, -1, 10, 3]
 
-let largestSubarraySum(array){
+function largestSubarraySum(array){
     let currentSum = 0
     let largestSum = 0
 
     for (let number of array){
-        
+
+
     }
+    return largestSum 
 }
